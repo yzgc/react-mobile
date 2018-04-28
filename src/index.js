eae1ea1491747e39ccf3e8e5eea92e5aa43fa861
yzgc <18317894461@163.com>
2018-04-28 18:22:47 +0800

    使用路由4.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, browerHistory } from 'react-router'
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 // 导入路由
 import { routes } from './route/route'
 console.log(routes)
 
 ReactDOM .render(
-    <Router history={browerHistory} routes={routes}></Router>,
+    <Router routes={routes}></Router>,
     document.getElementById('root')
 )
